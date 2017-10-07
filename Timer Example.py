46581ab9431baec8e5b142c92cb7bc2369f75b84
William <willtheorangeguy@users.noreply.github.com>
2017-10-07 12:16:38 -0700

    Add initial program files

---
+++ Timer Example.py
@@ -0,0 +1,24 @@
+import time
+
+# Main Time Function
+def timeStart():
+    # Variables to keep track and display
+    Sec = 0
+    Min = 0
+    # Begin Process
+    timeLoop = start
+    while timeLoop:
+        Sec += 1
+        print(str(Min) + " Min " + str(Sec) + " Secs ")
+        time.sleep(1)
+        if Sec == 59:
+            Sec = 0
+            Min += 1
+            print(str(Min) + " Minutes ")
+            if Min == 15:
+                break
+
+# Ask to Begin
+start = input("Would you like to begin Timing? (y/n): ")
+if start == "y":
+    timeStart()
