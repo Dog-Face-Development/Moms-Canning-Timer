864c0206d566fdf18d12652cbc63ca5df2a5af2d
William <willtheorangeguy@users.noreply.github.com>
2018-12-31 14:56:49 -0800

    Update the copyright dates on code to 2019

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 - 2018 Dog Face Development Company
+# Copyright (C) 2016 - 2019 Dog Face Development Co.
 
 # Import statements
 from tkinter import *
@@ -8,11 +8,13 @@ import time
 # Create Window
 window = Tk()
 window.title("Canning Timer")
-window.iconbitmap(r'D:\William\Documents\Dog Face Development Co\Projects\Canning Timer\favicon.ico')
+window.iconbitmap('favicon.ico')
 
 # Timer Program for Burner 1 
 def timeStart1():
     # Variables to keep track and display
+    global Sec
+    global Min
     Sec = 0
     Min = 0
     # Begin Process
@@ -77,6 +79,8 @@ def timeStart4():
             print(str(Min) + " Minutes ")
             if Min == 15:
                 break
+
+
 
 # Burner 1
 timer1title = Label(window, text = "#1")
