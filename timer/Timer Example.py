6da3e31a715a0ba883ba15435822f262962d1659
William <willtheorangeguy@users.noreply.github.com>
2022-09-03 21:27:31 -0700

    Add copyright text

--- timer/Timer Example.py
+++ timer/Timer Example.py
@@ -1,4 +1,20 @@
-# Copyright 2017-2022 Dog Face Development Co.
+"""
+Mom's Canning Timer - Customizable 15-minute stove top timers. 
+Copyright (C) 2017-2022 Dog Face Development Co.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import time
 
 # Main Time Function
